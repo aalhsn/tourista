2cd199a93854cf6c0dacc25f2f660602511d1c88
Abdullah A. Alhasan <a.a.alhasan95@gmail.com>
2019-08-26 02:14:17 +0300

    Abdullah Alhasan Blog

--- destinations/models.py
+++ destinations/models.py
@@ -7,3 +7,14 @@ class Destination(models.Model):
     pretty_picture = models.ImageField()
     activities = models.TextField()
     optimal_visiting_season = models.CharField(max_length=10)
+
+
+    def __str__(self):
+    	return self.name
+
+    def save(self, *args, **kwargs):
+	    for field_name in ['name', 'activities',"optimal_visiting_season"]:
+	        val = getattr(self, field_name, False)
+	        if val:
+	            setattr(self, field_name, val.capitalize())
+	    super(Destination, self).save(*args, **kwargs)
